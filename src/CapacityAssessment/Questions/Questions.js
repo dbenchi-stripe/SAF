310491b917077eec48c16b0884833410d96c6d5e
David Benchi <dbenchi@stripe.com>
2022-07-04 11:14:52 +0200

    add question wrapper

--- src/CapacityAssessment/Questions/Questions.js
+++ src/CapacityAssessment/Questions/Questions.js
@@ -11,21 +11,23 @@ export const Questions = () => {
   return (
     <div>
       <div className="question-card">
-        <div className="question-count">
-          Question <span> {currentQuestion + 1}</span> of{" "}
-          <span>{questions.length}</span>
-        </div>
-        <h2 className="question">{questions[currentQuestion].question}</h2>
-        <h3 className="question-text">
-          {questions[currentQuestion].ratingDefinition}
-        </h3>
-        {questions[currentQuestion].pathToGreen && (
-          <div className="path-to-green-container">
-            <h3 className="path-to-green">
-              {questions[currentQuestion].pathToGreen}
-            </h3>
+        <div className="question-wrapper">
+          <div className="question-count">
+            Question <span> {currentQuestion + 1}</span> of{" "}
+            <span>{questions.length}</span>
           </div>
-        )}
+          <h2 className="question">{questions[currentQuestion].question}</h2>
+          <h3 className="question-text">
+            {questions[currentQuestion].ratingDefinition}
+          </h3>
+          {questions[currentQuestion].pathToGreen && (
+            <div className="path-to-green-container">
+              <h3 className="path-to-green">
+                {questions[currentQuestion].pathToGreen}
+              </h3>
+            </div>
+          )}
+        </div>
 
         <Answers />
       </div>
