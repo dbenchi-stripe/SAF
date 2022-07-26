02c063d88cc6766a2c36a7e78b642ae1fa489487
David Benchi <dbenchi@stripe.com>
2022-07-26 15:19:54 +0200

    hide delivery guide

--- src/App.js
+++ src/App.js
@@ -2,24 +2,30 @@ import "@fontsource/roboto/300.css";
 import "@fontsource/roboto/400.css";
 import "@fontsource/roboto/500.css";
 import "@fontsource/roboto/700.css";
+import { isDevMode } from "helper/utils";
 import { useBeforeunload } from "react-beforeunload";
 import { Route, Routes } from "react-router-dom";
 
 import packageJson from "../package.json";
 import { Dashboard } from "./Dashboard/Dashboard";
 import { Introduction } from "./Introduction/Introduction";
+import { CapabilityAssessment } from "./StripeAdoptionFramework/CapacityAssessment/CapabilityAssessment";
 
 function App() {
   useBeforeunload((event) => event.preventDefault());
 
   console.log("You are using the version number: ", packageJson.version);
 
-  return (
-    <Routes>
-      <Route path="/" element={<Introduction />} />
-      <Route path="/saf" element={<Dashboard />} />
-    </Routes>
-  );
+  if (isDevMode()) {
+    return (
+      <Routes>
+        <Route path="/" element={<Introduction />} />
+        <Route path="/saf" element={<Dashboard />} />
+      </Routes>
+    );
+  } else {
+    return <CapabilityAssessment />;
+  }
 }
 
 export default App;
